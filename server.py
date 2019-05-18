05b82f05829bee38eeb11f80035da8be57252270
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-05-18 20:20:34 +0200

    Make subject a variable passed from req body

--- server.py
+++ server.py
@@ -23,26 +23,25 @@ def root ():
   if request.method == 'GET': 
     return 'ok', 200
   elif request.method == 'POST': 
-    email, date = request.get_json().values()
+    email, date, subject = request.get_json().values()
     # Check that email and date is provided
-    if email is None or date is None:
-      return 'Missing email / date', 500
+    if email is None or date is None or subject is None:
+      return 'Missing email / date / subject', 500
     # Format the provided date to ISO8601
     py_date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ').isoformat()
     # Check that the provided date has not already happened
     if py_date < datetime.now().isoformat(): 
       return 'Date has already happened', 500
     # Add send email job to schedule
-    scheduler.add_job(send_email, 'date', run_date=py_date, args=[email])
+    scheduler.add_job(send_email, 'date', run_date=py_date, args=[email, subject])
     return 'ok', 200
     
-def send_email (email): 
+def send_email (email, subject): 
   print('email: ', email)
   message = Mail(
     from_email=SENDGRID_SENDER,
     to_emails=email,
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    subject=subject)
   message.template_id = DEFAULT_TEMPLATE_ID
   try:
       sg = SendGridAPIClient(SENDGRID_API_KEY)
