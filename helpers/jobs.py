dc19e560a38049779561d49f04ee4d9e3f8b7a3c
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-08 17:57:13 +0200

    Add error_mail function Use error_mail as error handler for send_email

--- helpers/jobs.py
+++ helpers/jobs.py
@@ -5,6 +5,8 @@ from sendgrid.helpers.mail import Mail
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 SENDGRID_SENDER = os.getenv('SENDGRID_SENDER')
 DEFAULT_TEMPLATE_ID = os.getenv('DEFAULT_TEMPLATE_ID')
+DEFAULT_EMAIL_RECIPIENT = os.getenv('DEFAULT_EMAIL_RECIPIENT')
+ERROR_EMAIL_TEMPLATE = os.getenv('ERROR_EMAIL_TEMPLATE')
 
 def send_email (email, data, template=DEFAULT_TEMPLATE_ID): 
   message = Mail(
@@ -17,4 +19,16 @@ def send_email (email, data, template=DEFAULT_TEMPLATE_ID):
       response = sg.send(message)
       return response
   except Exception as e: 
-      print(e) 
+    if not data.isErrorMail:
+      error_email({
+        "subject": "Error when sending email",
+        "email": email,
+        "isErrorMail": True
+        })
+
+def error_email(data): 
+  send_email(
+    email=DEFAULT_EMAIL_RECIPIENT,
+    data=data,
+    template=ERROR_EMAIL_TEMPLATE
+  )
