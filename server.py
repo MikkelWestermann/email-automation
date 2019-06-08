4636b21b7e0da171c689a6596aa520194414e6c5
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-08 17:42:17 +0200

    Move send email function to a helper file Add debug level logging

--- server.py
+++ server.py
@@ -4,21 +4,18 @@ from flask import Flask, request
 app = Flask(__name__)
 
 from datetime import datetime 
- 
-# Sendgrid imports and config
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
-SENDGRID_SENDER = os.getenv('SENDGRID_SENDER')
-DEFAULT_TEMPLATE_ID = os.getenv('DEFAULT_TEMPLATE_ID')
+
+import logging
+logging.basicConfig(level=logging.DEBUG) 
+
+# import helper functions 
+from helpers import missed_job, error_in_job, send_email
 
 # APScheduler imports and config
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 # import event constants 
 from apscheduler.events import EVENT_JOB_MISSED, EVENT_JOB_ERROR
-# import job / schedule handlers from helpers 
-from helpers import missed_job, error_in_job
 
 # configure postgres jobstore
 POSTGRES_URI = os.getenv('POSTGRES_URI')
@@ -37,7 +34,7 @@ scheduler.add_listener(error_in_job, EVENT_JOB_ERROR)
 scheduler.start()
 
 @app.route('/', methods=['GET', 'POST'])
-def root (): 
+def root ():
   if request.method == 'GET': 
     return 'ok', 200
   elif request.method == 'POST': 
@@ -53,19 +50,6 @@ def root ():
     # Add send email job to schedule
     scheduler.add_job(send_email, 'date', run_date=py_date, args=[email, data])
     return 'ok', 200
-    
-def send_email (email, data): 
-  message = Mail(
-    from_email=SENDGRID_SENDER,
-    to_emails=email)
-  message.dynamic_template_data = data
-  message.template_id = DEFAULT_TEMPLATE_ID
-  try:
-      sg = SendGridAPIClient(SENDGRID_API_KEY)
-      response = sg.send(message)
-      return response
-  except Exception as e: 
-      print(e) 
 
 if __name__ == '__main__': 
   app.run(host='0.0.0.0', port=os.getenv('DEFAULT_PORT') or 5000)
