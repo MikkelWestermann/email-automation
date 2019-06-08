84b412935ee7c112d3be9f8167a635e4efcac63d
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-08 18:01:17 +0200

    Add debug logging for job error handlers

--- helpers/job_error_handlers.py
+++ helpers/job_error_handlers.py
@@ -1,5 +1,9 @@
-def missed_job():
-  print('missed job...')
+import logging 
+logger = logging.getLogger(__name__)
+def missed_job(event):
+  logger.debug('*Missed Job*')
+  logger.debug(event)
 
-def error_in_job(): 
-  print('error in job...')
+def error_in_job(event): 
+  logger.debug('*An error occurred in job*')
+  logger.debug(event)
