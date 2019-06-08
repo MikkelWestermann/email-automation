849fd49c8fbacdd9e028ea4a81c2773017fbac8c
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-08 17:47:30 +0200

    Add template to parameters in send_email function

--- helpers/jobs.py
+++ helpers/jobs.py
@@ -6,12 +6,12 @@ SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 SENDGRID_SENDER = os.getenv('SENDGRID_SENDER')
 DEFAULT_TEMPLATE_ID = os.getenv('DEFAULT_TEMPLATE_ID')
 
-def send_email (email, data): 
+def send_email (email, data, template=DEFAULT_TEMPLATE_ID): 
   message = Mail(
     from_email=SENDGRID_SENDER,
     to_emails=email)
   message.dynamic_template_data = data
-  message.template_id = DEFAULT_TEMPLATE_ID
+  message.template_id = template
   try:
       sg = SendGridAPIClient(SENDGRID_API_KEY)
       response = sg.send(message)
